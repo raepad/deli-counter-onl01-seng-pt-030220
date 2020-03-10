9883835f64eebf8a61f6667931c82c91f04691c9
Rachel Padworski <rachelpadworski@gmail.com>
2020-03-10 01:17:49 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,8 +3,8 @@ def line(katz_deli)
   if katz_deli.length == 0 
     puts "The line is currently empty."
   else
-    katz_deli.each_with_index(1) do |name, index|
-      line_position.push("#{index}. #{name}")
+    katz_deli.each_with_index do |name, index|
+      line_position.push("#{index + 1}. #{name}")
     end
     puts "The line is currently: " << "#{line_position.join(" ")}"
   end
