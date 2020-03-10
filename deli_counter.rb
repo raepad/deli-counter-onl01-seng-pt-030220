cc481871eb26a05e6b267fd67684b81c274cfa0a
Rachel Padworski <rachelpadworski@gmail.com>
2020-03-10 01:26:33 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,7 +13,7 @@ end
 def take_a_number (katz_deli, name)
   katz_deli << name
   katz_deli.each_with_index do |name, index|
-    puts "Welcome, #{name}. You are number #{index + 1} in line."
+    puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
   end
 end
 
