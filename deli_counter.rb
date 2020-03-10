938f0ddc914da11eed8244e242b14b9b74bcb522
Rachel Padworski <rachelpadworski@gmail.com>
2020-03-10 00:32:29 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -22,8 +22,9 @@ def take_a_number (katz_deli, name)
 end
 
 def now_serving(katz_deli, name)
-  katz_deli << name
-  puts "Welcome #{name}. You are number #{katz_deli.length} in line."
+ # katz_deli << name
+  #puts "Welcome #{name}. You are number #{katz_deli.length} in line."
+  
   #if katz_deli == 0 
   #  puts "There is nobody waiting to be served!"
   #else
